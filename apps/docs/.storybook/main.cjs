1dedf6c65983943205daadc3acbc233b2673c0ab
이상혁 <desummit1@gmail.com>
2023-03-13 21:01:37 +0900

    ✨ Feat: Storybook 설치 및 환경 설정
    - Storybook 공용 페이지를 위한 docs 생성
    - 공용 컴포넌트 확인을 위한 packages/ui 환경 설정 완료

---
+++ apps/docs/.storybook/main.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  refs: [
+    {
+      title: "Ui",
+      url: "ui/",
+      url:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:6008/"
+          : "ui/",
+    },
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
